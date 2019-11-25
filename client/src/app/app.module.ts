7fb3bafe8780c762a03480cdea0a41f3f5e8eaef
Vince <vvu9@uwo.ca>
2019-11-25 12:24:29 -0500

    Reverting Back to Lab 4 front end

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { ListComponent } from "./list/list.component";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { AdminComponent } from "./admin/admin.component";
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, ListComponent, AdminComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
